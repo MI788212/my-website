2d3d6079dded27397cdfdb395808096bc5ef3f05
MI <ivana.minova@students.finki.ukim.mk>
2025-06-26 22:58:17 +0200

    mn stvari

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
-
 function showImage(src) {
     const lightbox = document.getElementById("lightbox");
     const img = document.getElementById("lightbox-img");
+    const loadingMessage = document.getElementById("loading-message");
 
     img.style.display = "none";
+    loadingMessage.style.display = "block";//da e jasno deka se loadira
     img.onload = () => {
+        loadingMessage.style.display = "none";
         img.style.display = "block";
     }; //popravam ghost image
 
